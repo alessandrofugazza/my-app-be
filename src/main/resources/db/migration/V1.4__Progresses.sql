cc5be7f7c2ba62d639b599450b7754e33ce67279
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-08-08 07:11:30 +0200

    fixes Progress .sql

--- src/main/resources/db/migration/V1.4__Progresses.sql
+++ src/main/resources/db/migration/V1.4__Progresses.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS learn_subjects (
+CREATE TABLE IF NOT EXISTS progresses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     source VARCHAR(255),
     progress VARCHAR(255),
